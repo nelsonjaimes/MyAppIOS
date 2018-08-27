728fb3032366f0bebdf8cfab1a0b1a2bc2c6e9bc
Nelson Jaimes Gonzales <nelsonjaimesgonzales@gmail.com>
2018-08-26 22:02:31 -0500

    UIAlertContoller and UIAllertAction

--- MyAppIOs/ViewController.swift
+++ MyAppIOs/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnValide: UIButton!
+    @IBOutlet weak var lbIntent: UILabel!
+    @IBOutlet weak var tfNumber: UITextField!
+    @IBOutlet weak var lbMessage: UILabel!
+    var ramdonNumber = ""
+    var intents = 0
+    
+    @IBAction func btnValide(_ sender: Any) {
+        if tfNumber.text == ramdonNumber{
+            self.view.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+            showAlertDialog(content: "Felicidades, Ganaste!!")
+        }else{
+            if(intents == 1){
+                self.view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+                intents = 0
+                lbIntent.text = String(intents)
+                showAlertDialog(content: "Agostaste tus 5 intentos")
+            }else{
+                intents -= 1
+                lbIntent.text = String(intents)
+                tfNumber.text = ""
+                lbMessage.text = "Fallaste, intenta de nuevo"
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       setVAlues()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setVAlues(){
+        ramdonNumber = String(arc4random_uniform(10))
+        print(ramdonNumber)
+        intents = 5
+        lbIntent.text = String(intents)
+        tfNumber.text = ""
+        lbMessage.text = "Ingresa un número del 0-9"
+        self.view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
-
-
+    
+    func showAlertDialog(content:String){
+        let alert = UIAlertController(title: content, message: "", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Juegar de nuevo", style: .default) { (UIAlertAction) in
+            self.setVAlues()
+        }
+        alert.addAction(alertAction)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
